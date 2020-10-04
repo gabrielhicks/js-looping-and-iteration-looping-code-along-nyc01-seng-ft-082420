45e63d4231770eb7e7f97e941c3bbbe37b150fee
gabrielhicks <gabrielhicks@mail.com>
2020-10-04 15:09:24 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(array, string) {
+    const thanks = []
+
+    for (let i = 0; i < array.length; i++) {
+        thanks.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+        // console.log(`Thank you ${array}`)
+    }
+
+    return thanks
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i)
+    }
+}
+
+// writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise')
